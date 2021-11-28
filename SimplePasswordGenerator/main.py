b28a411c3a2e9db440bbbb75fd39db78f6857ef5
lnrdmnc <jlrknumber1@gmail.com>
2021-11-28 02:51:59 +0100

    Insert SimplePasswordGenerator a script in python that generate randomly passwor

---
+++ SimplePasswordGenerator/main.py
@@ -0,0 +1,29 @@
+import random
+
+
+uppercase_letter="ABCDEFGHILMNOPQRSZUWXYZ"
+lowercase_letters="abcdefghilmnopqrstuwxyz" ##uppercase_letter.lower()
+numbers="0123456789"
+symbols="()!$%&/=?^\\*+#@"
+
+upper,lower,num,sym= True,True,True,True
+
+all=""
+
+if upper:
+    all += uppercase_letter
+if lower:
+    all+= lowercase_letters
+if num:
+    all+= numbers
+if sym:
+    all += symbols
+
+lenght=15
+amount=10
+with open("passwordGenerator.txt", "w") as f:
+    for x in range(amount):
+        password= "".join(random.sample(all,lenght))
+        f.write(password)
+        f.write("\n")
+
